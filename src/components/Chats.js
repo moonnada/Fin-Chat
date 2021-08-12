c40c61ed2a339ef6fdbfa348ab2398dfccb04e7c
Byung Moon Kim <kimbm2014@gmail.com>
2021-08-12 00:19:08 -0700

    created chats.js for a chat main page

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import {ChatEngine} from 'react-chat-engine';
+import {auth} from '../firebase';
+import { useAuth } from '../contexts/AuthContext';
 
 const Chats = () => {
+    const history = useHistory();
+    const {user} = useAuth();
+
+    console.log(user);
+
+    const handleLogout = async () => {
+        await auth.signOut();
+        history.push('/');
+    }
+
     return (
-        <h1>Chats</h1>
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Unichat
+                </div>
+                <div onClick={handleLogout} className="logout-tab">
+                    Logout
+                </div>
+            </div>
+
+            <ChatEngine 
+                height="calc(100vh - 66px)"
+                projectId="b502a849-e81a-4a0c-85e3-7e33dc04ec28"
+                userName="."
+                userSecret="."
+
+            />
+        </div>
+
     );
 }
 
